6e5ff35b9027aff9d18079b0e1a4ec6da40771ef
Rafael Santos <rafaelnsantos1@gmail.com>
2019-04-25 16:21:40 -0300

    fix graphql path

--- src/schema.js
+++ src/schema.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const { makeExecutableSchema } = require('graphql-tools');
 const glue = require('schemaglue');
 
-const { schema, resolver } = glue('src/graphql');
+const { schema, resolver } = glue(path.join(__dirname, 'graphql'));
 
 const directiveResolvers = require('./graphql/directives');
 
