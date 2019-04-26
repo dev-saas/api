4fc3159478c10d7ff1aefffd87154615ca860737
Rafael Santos <rafaelnsantos1@gmail.com>
2019-04-26 15:24:18 -0300

    add mqtt graphql

---
+++ src/graphql/resolvers/mqtt.js
@@ -0,0 +1,18 @@
+exports.resolver = {
+  Mutation: {
+    turnOn: async (_, { relay }, { mqtt }) => {
+      await mqtt.publish('/turnOn', relay.toString());
+      return true;
+    },
+    turnOff: async (_, { relay }, { mqtt }) => {
+      await mqtt.publish('/turnOff', relay.toString());
+      return true;
+    }
+  },
+  Query: {
+    status: async (_, args, { mqtt }) => {
+      await mqtt.publish('/status');
+      return true;
+    }
+  }
+};
--- src/graphql/subscriptions/subscriptions.js
+++ src/graphql/subscriptions/subscriptions.js
@@ -1,4 +1,11 @@
-const { NEW_EVENT, UPDATED_EVENT } = require('./channels');
+const {
+  NEW_EVENT,
+  UPDATED_EVENT,
+  NEW_HUMIDITY,
+  NEW_TEMPERATURE,
+  RELAY_1,
+  RELAY_2
+} = require('./channels');
 
 exports.resolver = {
   Subscription: {
@@ -7,6 +14,18 @@ exports.resolver = {
     },
     updatedEvent: {
       subscribe: (_, args, { pubsub }) => pubsub.asyncIterator(UPDATED_EVENT)
+    },
+    newTemperature: {
+      subscribe: (_, args, { pubsub }) => pubsub.asyncIterator(NEW_TEMPERATURE)
+    },
+    newHumidity: {
+      subscribe: (_, args, { pubsub }) => pubsub.asyncIterator(NEW_HUMIDITY)
+    },
+    relay1: {
+      subscribe: (_, args, { pubsub }) => pubsub.asyncIterator(RELAY_1)
+    },
+    relay2: {
+      subscribe: (_, args, { pubsub }) => pubsub.asyncIterator(RELAY_2)
     }
   }
 };
