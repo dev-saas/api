b8dee871c676d8af38492fcc97e7cec7859b0b70
Rafael Santos <rafaelnsantos1@gmail.com>
2019-04-30 01:17:17 -0300

    refactor

--- src/graphql/subscriptions/subscriptions.js
+++ src/graphql/subscriptions/subscriptions.js
@@ -1,10 +1,4 @@
-const {
-  NEW_EVENT,
-  UPDATED_EVENT,
-  TURNED_OFF,
-  TURNED_ON,
-  NEW_VALUE
-} = require('./channels');
+const { NEW_EVENT, UPDATED_EVENT, NEW_VALUE } = require('./channels');
 
 exports.resolver = {
   Subscription: {
@@ -13,12 +7,6 @@ exports.resolver = {
     },
     updatedEvent: {
       subscribe: (_, args, { pubsub }) => pubsub.asyncIterator(UPDATED_EVENT)
-    },
-    turnedOn: {
-      subscribe: (_, args, { pubsub }) => pubsub.asyncIterator(TURNED_ON)
-    },
-    turnedOff: {
-      subscribe: (_, args, { pubsub }) => pubsub.asyncIterator(TURNED_OFF)
     },
     newValue: {
       subscribe: (_, args, { pubsub }) => pubsub.asyncIterator(NEW_VALUE)
--- src/mqtt.js
+++ src/mqtt.js
@@ -1,31 +1,44 @@
 const mqtt = require('mqtt');
 const pubsub = require('./pubsub');
 
-const {
-  TURNED_OFF,
-  TURNED_ON,
-  NEW_VALUE
-} = require('./graphql/subscriptions/channels');
+const { NEW_VALUE } = require('./graphql/subscriptions/channels');
 
 const client = mqtt.connect(process.env.CLOUDMQTT_URL);
 
 client.on('connect', () => {
-  client.subscribe('temperatura');
-  client.subscribe('umidade');
+  client.subscribe('temperature');
+  client.subscribe('humidity');
   client.subscribe('turnedOn');
   client.subscribe('turnedOff');
-  client.subscribe('luminosidade');
+  client.subscribe('light');
   client.subscribe('moisture');
+  client.subscribe('report');
 });
 
 client.on('message', function(topic, msg, pkt) {
-  const value = JSON.parse(msg);
+  try {
+    var value = JSON.parse(msg);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
   switch (topic) {
     case 'turnedOn':
-      pubsub.publish(TURNED_ON, { turnedOn: value });
+      pubsub.publish(NEW_VALUE, {
+        newValue: { name: `relay${value}`, value: true }
+      });
       break;
     case 'turnedOff':
-      pubsub.publish(TURNED_OFF, { turnedOff: value });
+      pubsub.publish(NEW_VALUE, {
+        newValue: { name: `relay${value}`, value: false }
+      });
+      break;
+    case 'report':
+      Object.entries(value).map(entry =>
+        pubsub.publish(NEW_VALUE, {
+          newValue: { name: entry[0], value: entry[1] }
+        })
+      );
       break;
     default:
       pubsub.publish(NEW_VALUE, {
