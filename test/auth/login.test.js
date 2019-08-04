776ef2722146cb386b0f05bbb0185eeea926fb6b
Rafael <rafaelnsantos1@gmail.com>
2019-08-04 13:05:10 -0300

    test

---
+++ test/auth/login.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+
+// const request = require('supertest')
+// const { queryLogin } = require('./queries')
+// const app = require('../../src/app')
+
+describe('An user', () => {
+  // it('should login with right credentials', () => {
+  //   queryLogin.variables = {
+  //     email: 'test@test.com',
+  //     password: 'test'
+  //   }
+  //   return request(app)
+  //     .post('/graphql')
+  //     .set('recaptcha', 'test')
+  //     .send(queryLogin)
+  //     .expect(200)
+  //     .expect(res => {
+  //       expect(res.body.errors).toBeUndefined()
+  //       expect(res.body.data.token).toBeDefined()
+  //     })
+  // })
+  it('should pass', () => {
+    return true
+  })
+})
