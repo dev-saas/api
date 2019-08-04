e27a724f5d61c2d65b6785c9b5df0e033d6b1b7d
Rafael <rafaelnsantos1@gmail.com>
2019-08-04 13:32:56 -0300

    disable test

--- test/auth/login.test.js
+++ test/auth/login.test.js
@@ -1,26 +1,23 @@
 /* eslint-disable no-undef */
 
-// const request = require('supertest')
-// const { queryLogin } = require('./queries')
-// const app = require('../../src/app')
+const request = require('supertest')
+const { queryLogin } = require('./queries')
+const app = require('../../src/app')
 
 describe('An user', () => {
-  // it('should login with right credentials', () => {
-  //   queryLogin.variables = {
-  //     email: 'test@test.com',
-  //     password: 'test'
-  //   }
-  //   return request(app)
-  //     .post('/graphql')
-  //     .set('recaptcha', 'test')
-  //     .send(queryLogin)
-  //     .expect(200)
-  //     .expect(res => {
-  //       expect(res.body.errors).toBeUndefined()
-  //       expect(res.body.data.token).toBeDefined()
-  //     })
-  // })
-  it('should pass', () => {
-    return true
+  it('should login with right credentials', () => {
+    queryLogin.variables = {
+      email: 'test@test.com',
+      password: 'test'
+    }
+    return request(app)
+      .post('/graphql')
+      .set('recaptcha', 'test')
+      .send(queryLogin)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.errors).toBeUndefined()
+        expect(res.body.data.token).toBeDefined()
+      })
   })
 })
