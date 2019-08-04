33ae39af0fecc86a89ea75e712a6bee9eec8d683
Rafael <rafaelnsantos1@gmail.com>
2019-08-04 14:09:11 -0300

    firebasefix

--- src/services/firebase-service.js
+++ src/services/firebase-service.js
@@ -5,7 +5,9 @@ admin.initializeApp({
     type: 'service_account',
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: `-----BEGIN PRIVATE KEY-----\n${
+      process.env.FIREBASE_PRIVATE_KEY
+    }\n-----END PRIVATE KEY-----\n`,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
