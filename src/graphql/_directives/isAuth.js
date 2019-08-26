a62aae036019595587e13d49e14a5185524734ff
Rafael Santos <rafaelnsantos1@gmail.com>
2019-08-25 21:40:14 -0300

    fix auth directive

--- src/graphql/_directives/isAuth.js
+++ src/graphql/_directives/isAuth.js
@@ -4,7 +4,7 @@ module.exports = {
   isAuth (next, _, require, { user }) {
     if (!user) throw new Error(UNAUTHENTICATED)
 
-    if (require && require.roles.length > 0 && !require.roles.includes(user.role)) {
+    if (require.roles && require.roles.length > 0 && !require.roles.includes(user.role)) {
       throw new Error(UNAUTHORIZED)
     }
 
