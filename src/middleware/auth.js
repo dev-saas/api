d4a1ca11e564ad8aa609a80326c389a984f829e3
Rafael Santos <rafaelnsantos1@gmail.com>
2019-08-26 23:24:26 -0300

    auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,7 @@
 const { auth } = require('../services/firebase-service')
 
 module.exports = async token => {
+  if (!token) return null
   try {
     const { uid } = await auth.verifyIdToken(token)
     return uid
