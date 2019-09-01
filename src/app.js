6b2117b7711eae8ea02aecc6d2afedecfcee28a8
Rafael Santos <rafaelnsantos1@gmail.com>
2019-09-01 16:09:54 -0300

    remove body-parser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const apolloServer = require('./apollo-server')
-const bodyParser = require('body-parser')
+
 const app = express()
-app.use(bodyParser.json())
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
